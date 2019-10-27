5a2d4bc90715f987db6ac09f98f53a1c74344439
Baptiste Bouchereau <baptiste.bouchereau@gmail.com>
2019-10-27 22:11:22 +0100

    Get transactions as json

--- modules/ccmparser.py
+++ modules/ccmparser.py
@@ -1,15 +1,41 @@
 import sys
 sys.path.append('./modules')
 from linereader import LineReader
+from pdfparser import PdfParser
 
 class CCMParser:
 
     def __init__(self, lines):
         self.lines = lines
         self.currentAccount = None
+        self.currentColumnBoundaries = None
         self.data = []
 
-    # def parse(self):
+    def setColumnBoundaries(self, line):
+        self.currentColumnBoundaries = self.getColumnBoundaries(line)
+
+    def parse(self):
+        transactions = {}
+
+        for index, line in enumerate(self.lines):
+
+            if self.isAccountNameLine(index, self.lines):
+                self.currentAccount = self.extractAccountName(line)
+                if self.currentAccount not in transactions:
+                    transactions[self.currentAccount] = []
+                continue
+
+            if self.isHeaderTableLine(line):
+                self.setColumnBoundaries(line)
+                continue
+
+            if (self.currentAccount is not None and self.currentColumnBoundaries is not None):
+                if self.isDebitLine(line):
+                    transactions[self.currentAccount].append(self.extractTransaction(index, self.lines, 'debit'))
+                elif self.isCreditLine(line):
+                    transactions[self.currentAccount].append(self.extractTransaction(index, self.lines, 'credit'))
+
+        return transactions
 
     def isHeaderTableLine(self, line):
         if (
@@ -22,6 +48,80 @@ class CCMParser:
         ):
             return True
         return False
+
+    def isAccountNameLine(self, lineIndex, lines):
+        line1Reader = LineReader(self.lines[lineIndex])
+        if line1Reader.contains('en euros'):
+            line2Reader = LineReader(self.lines[lineIndex + 1])
+            if line2Reader.contains('TITULAIRE(S)'):
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def isDebitLine(self, line):
+        # todo add regexes
+        if (
+            len(line) >= 4 and
+            self.linesAreWithinBoundaries(line[0], self.currentColumnBoundaries['date']) and
+            self.linesAreWithinBoundaries(line[1], self.currentColumnBoundaries['date_valeur']) and
+            self.linesAreWithinBoundaries(line[2], self.currentColumnBoundaries['operation']) and
+            self.linesAreWithinBoundaries(line[3], self.currentColumnBoundaries['debit'])
+        ):
+            return True
+        else:
+            return False
+
+    def isCreditLine(self, line):
+        # todo add regexes
+        if (
+            len(line) >= 4 and
+            self.linesAreWithinBoundaries(line[0], self.currentColumnBoundaries['date']) and
+            self.linesAreWithinBoundaries(line[1], self.currentColumnBoundaries['date_valeur']) and
+            self.linesAreWithinBoundaries(line[2], self.currentColumnBoundaries['operation']) and
+            self.linesAreWithinBoundaries(line[3], self.currentColumnBoundaries['credit'])
+        ):
+            return True
+        else:
+            return False
+
+    def extractAccountName(self, line):
+        # index 0 as the account name line contains always only one item
+        return line[0]['value'][:-len(' en euros')]
+
+    def extractTransaction(self, lineIndex, lines, transactionType):
+
+        value = float(
+          lines[lineIndex][3]['value']
+          .replace('.', '')
+          .replace(',', '.')
+        )
+
+        transaction = {
+            'date': lines[lineIndex][0]['value'],
+            'label': lines[lineIndex][2]['value'],
+            'value': value if transactionType == 'credit' else -value
+        }
+
+        startIndex = lineIndex + 1
+        for line in lines[startIndex:]:
+            if self.linesAreWithinBoundaries(line[0], self.currentColumnBoundaries['operation']) and len(line) == 1:
+                transaction['label'] = transaction.get('label') + ' ' + line[0]['value']
+            else:
+                break
+
+        return transaction
+
+    def linesAreWithinBoundaries(self, line1, line2):
+        # allow a margin of 0.05
+        if (
+            (line1['x0']-line2['x0'] >= -0.05 and line2['x1']-line1['x1'] >= -0.05) or
+            (line2['x0']-line1['x0'] >= -0.05 and line1['x1']-line2['x1'] >= -0.05)
+        ):
+            return True
+        else:
+            return False
 
     def getColumnBoundaries(self, line):
         boundaries = {}
@@ -40,14 +140,3 @@ class CCMParser:
             }
 
         return boundaries
-
-    def isAccountNameLine(self, lineIndex):
-        line1Reader = LineReader(self.lines[lineIndex])
-        if line1Reader.contains('en euros'):
-            line2Reader = LineReader(self.lines[lineIndex + 1])
-            if line2Reader.contains('TITULAIRE(S)'):
-                return True
-            else:
-                return False
-        else:
-            return False
--- tests/ccmparser.test.py
+++ tests/ccmparser.test.py
@@ -1,6 +1,8 @@
 import sys
 sys.path.append('./modules')
 from ccmparser import CCMParser
+from pdfparser import PdfParser
+import json
 import unittest
 
 # import ptvsd
@@ -9,7 +11,37 @@ import unittest
 
 class TestCCMParser(unittest.TestCase):
 
-    def testLineContains(self):
+    def testParse(self):
+        print("\nAssert the file is correctly parsed")
+
+        pdfFile = open('./tests/releve-credit-mutuel.pdf', 'rb')
+        self.lines = PdfParser().parse(pdfFile)
+        pdfFile.close()
+
+        ccmparser = CCMParser(self.lines)
+        transactions = ccmparser.parse()
+        pdfFile.close()
+
+        with open('./tests/expected-results.json') as file:
+            expectedData = json.loads(file.read())
+
+        with open('./tests/received-results.json', 'w', encoding='utf-8') as file:
+            json.dump(transactions, file, ensure_ascii=False, indent=4)
+
+        self.assertEqual(transactions, expectedData)
+
+    def testExtractAccountName(self):
+        print("\nAssert the account name is correctly extracted")
+        accountLines = [
+            [{'value': 'C/C EUROCOMPTE JEUNE N° 00020324201 en euros', 'x0': 81.6, 'y0': 514.55, 'x1': 331.79, 'y1': 502.65}],
+            [{'value': 'LIVRET BLEU N° 00020324203 en euros', 'x0': 81.6, 'y0': 694.55, 'x1': 269.56, 'y1': 682.65}]
+        ]
+
+        ccmParser = CCMParser(accountLines)
+        self.assertEqual(ccmParser.extractAccountName(accountLines[0]), 'C/C EUROCOMPTE JEUNE N° 00020324201')
+        self.assertEqual(ccmParser.extractAccountName(accountLines[1]), 'LIVRET BLEU N° 00020324203')
+
+    def testIsAccountNameLine(self):
         print("\nAssert the line is an account name line")
 
         accountLines = [
@@ -33,11 +65,11 @@ class TestCCMParser(unittest.TestCase):
         ]
 
         ccmParser = CCMParser(accountLines)
-        self.assertEqual(ccmParser.isAccountNameLine(0), False)
-        self.assertEqual(ccmParser.isAccountNameLine(1), True)
-        self.assertEqual(ccmParser.isAccountNameLine(2), False)
-        self.assertEqual(ccmParser.isAccountNameLine(3), False)
-        self.assertEqual(ccmParser.isAccountNameLine(4), False)
+        self.assertEqual(ccmParser.isAccountNameLine(0, accountLines), False)
+        self.assertEqual(ccmParser.isAccountNameLine(1, accountLines), True)
+        self.assertEqual(ccmParser.isAccountNameLine(2, accountLines), False)
+        self.assertEqual(ccmParser.isAccountNameLine(3, accountLines), False)
+        self.assertEqual(ccmParser.isAccountNameLine(4, accountLines), False)
 
     def testIsHeaderTableLine(self):
         headerTableLines = [
@@ -67,7 +99,7 @@ class TestCCMParser(unittest.TestCase):
         self.assertEqual(ccmParser.isHeaderTableLine(headerTableLines[2]), False)
 
     def testGetColumnBoundaries(self):
-        print("\Assert we get the right table column x boundaries from the data")
+        print("\nAssert we get the right table column x boundaries from the data")
 
         headerTableLine = [
             {'value': 'Date', 'x0': 63.36, 'x1': 80.7, 'y0': 484.98, 'y1': 475.46},
@@ -105,7 +137,15 @@ class TestCCMParser(unittest.TestCase):
     def testLineIsDebit(self):
         print("\nAssert the line is a debit line")
 
-        debitLines = [
+        headerTableLine = [
+            {'value': 'Date', 'x0': 63.36, 'x1': 80.7, 'y0': 484.98, 'y1': 475.46},
+            {'value': 'Date valeur', 'x0': 98.4, 'x1': 141.53, 'y0': 484.98, 'y1': 475.46},
+            {'value': 'Opération', 'x0': 160.8, 'x1': 198.58, 'y0': 484.98, 'y1': 475.46},
+            {'value': 'Débit euros', 'x0': 415.68, 'x1': 459.69, 'y0': 484.98, 'y1': 475.46},
+            {'value': 'Crédit euros', 'x0': 486.96, 'x1': 534.08, 'y0': 484.98, 'y1': 475.46}
+        ]
+
+        lines = [
             [
                 {'value': '02/05/2014', 'x0': 52.08, 'x1': 92.11, 'y0': 448.47, 'y1': 439.22},
                 {'value': '02/05/2014', 'x0': 100.08, 'x1': 140.11, 'y0': 448.47, 'y1': 439.22},
@@ -114,11 +154,58 @@ class TestCCMParser(unittest.TestCase):
             ],
             [
                 {'value': 'EUROPA PARK GMBH CA... 02892630', 'x0': 147.6, 'x1': 298.76, 'y0': 437.91, 'y1': 428.66}
+            ],
+            [
+                {'value': '03/05/2014', 'x0': 52.08, 'x1': 92.11, 'y0': 321.75, 'y1': 312.5},
+                {'value': '03/05/2014', 'x0': 100.08, 'x1': 140.11, 'y0': 321.75, 'y1': 312.5},
+                {'value': 'VIRT PARENTS', 'x0': 147.6, 'x1': 205.83, 'y0': 321.75, 'y1': 312.5},
+                {'value': '350,00', 'x0': 509.52, 'x1': 533.98, 'y0': 321.75, 'y1': 312.5}
             ]
         ]
 
-        ccmParser = CCMParser(debitLines)
-        # self.assertEqual(ccmParser.isAccountNameLine(0), False)
+        ccmParser = CCMParser(lines)
+        ccmParser.setColumnBoundaries(headerTableLine)
+        self.assertEqual(ccmParser.isDebitLine(lines[0]), True)
+        self.assertEqual(ccmParser.isDebitLine(lines[1]), False)
+        self.assertEqual(ccmParser.isDebitLine(lines[2]), False)
+
+    def testExtractTransaction(self):
+        print("\nAssert we extract data from transaction lines")
+
+        headerTableLine = [
+            {'value': 'Date', 'x0': 63.36, 'x1': 80.7, 'y0': 484.98, 'y1': 475.46},
+            {'value': 'Date valeur', 'x0': 98.4, 'x1': 141.53, 'y0': 484.98, 'y1': 475.46},
+            {'value': 'Opération', 'x0': 160.8, 'x1': 198.58, 'y0': 484.98, 'y1': 475.46},
+            {'value': 'Débit euros', 'x0': 415.68, 'x1': 459.69, 'y0': 484.98, 'y1': 475.46},
+            {'value': 'Crédit euros', 'x0': 486.96, 'x1': 534.08, 'y0': 484.98, 'y1': 475.46}
+        ]
+
+        lines = [
+            [
+                {'value': '02/05/2014', 'x0': 52.08, 'x1': 92.11, 'y0': 448.47, 'y1': 439.22},
+                {'value': '02/05/2014', 'x0': 100.08, 'x1': 140.11, 'y0': 448.47, 'y1': 439.22},
+                {'value': 'PAIEMENT CB 0105 RUST', 'x0': 147.6, 'x1': 248.07, 'y0': 448.47, 'y1': 439.22},
+                {'value': '29,50', 'x0': 439.68, 'x1': 459.7, 'y0': 448.47, 'y1': 439.22}
+            ],
+            [
+                {'value': 'EUROPA PARK GMBH CARTE 02892630', 'x0': 147.6, 'x1': 298.76, 'y0': 437.91, 'y1': 428.66}
+            ],
+            [
+                {'value': '03/05/2014', 'x0': 52.08, 'x1': 92.11, 'y0': 321.75, 'y1': 312.5},
+                {'value': '03/05/2014', 'x0': 100.08, 'x1': 140.11, 'y0': 321.75, 'y1': 312.5},
+                {'value': 'VIRT PARENTS', 'x0': 147.6, 'x1': 205.83, 'y0': 321.75, 'y1': 312.5},
+                {'value': '350,00', 'x0': 509.52, 'x1': 533.98, 'y0': 321.75, 'y1': 312.5}
+            ]
+        ]
+
+        ccmParser = CCMParser(lines)
+        ccmParser.setColumnBoundaries(headerTableLine)
+
+        self.assertDictEqual(ccmParser.extractTransaction(0, lines, 'debit'), {
+            'date': '02/05/2014',
+            'label': 'PAIEMENT CB 0105 RUST EUROPA PARK GMBH CARTE 02892630',
+            'value': -29.50
+        })
 
 if __name__ == '__main__':
     unittest.main()
