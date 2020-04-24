7dc018de88ddf659bbb578b58c686d4974fe0f47
Baptiste Bouchereau <baptiste.bouchereau@gmail.com>
2020-04-24 10:29:36 +0200

    Add the caisse epargne parser route in the api

--- run_api.py
+++ run_api.py
@@ -5,23 +5,38 @@ import uuid
 from datetime import datetime
 sys.path.append('modules')
 from credit_mutuel_statement_parser import CreditMutuelStatementParser
+from caisse_epargne_statement_parser import CaisseEpargneStatementParser
 from pdf_parser import PdfParser
 from klein import run, route
 
-def parse(file_path):
+def parse(file_path, parser_name):
     pdfFile = open(file_path, 'rb')
     lines = PdfParser().parse(pdfFile)
     pdfFile.close()
-    ccmparser = CreditMutuelStatementParser(lines)
-    transactions = ccmparser.parse()
+
+    if (parser_name == 'credit-mutuel'):
+        parser = CreditMutuelStatementParser(lines)
+    elif (parser_name == 'caisse-epargne'):
+        parser = CaisseEpargneStatementParser(lines)
+    else:
+        raise Exception('Unknown parser with name' + parser_name)
+
+    transactions = parser.parse()
 
     return transactions
 
-@route('/')
-def statement(request):
+def generate_response(request, parser_name):
     file_path = request.args[b'statement'][0] 
-    transactions = parse(file_path)
+    transactions = parse(file_path, 'credit-mutuel')
 
     return json.dumps(transactions, indent=2, ensure_ascii=False)
+
+@route('/credit-mutuel/')
+def statement(request):
+    return generate_response(request, 'credit-mutuel')
+
+@route('/caisse-epargne/')
+def statement(request):
+    return generate_response(request, 'caisse-epargne')
 
 run('0.0.0.0', 80)
