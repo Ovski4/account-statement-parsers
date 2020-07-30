1774d6f1fdd7f69e3ae3232ed0fbe6956c5f4dfe
Baptiste Bouchereau <baptiste.bouchereau@gmail.com>
2020-07-30 22:16:57 +0200

    Parse N26 account statement data

--- modules/n26_statement_parser.py
+++ modules/n26_statement_parser.py
@@ -7,11 +7,98 @@ from pdf_parser import PdfParser
 class N26StatementParser:
 
     def __init__(self, lines):
+        self.dateRegex  = re.compile(
+            r'(lundi|mardi|mercredi|jeudi|vendredi|samedi|dimanche), '
+            r'(\d+)\. '
+            r'(janvier|février|mars|avril|mai|juin|juillet|août|septembre|octobre|novembre|décembre)'
+            r' (\d+)'
+        )
+        self.valueRegex = r'(-|\+)(\d+),(\d{2})€'
+        self.months = {
+            'janvier': '01',
+            'février': '02',
+            'mars': '03',
+            'avril': '04',
+            'mai': '05',
+            'juin': '06',
+            'juillet': '07',
+            'août': '08',
+            'septembre': '09',
+            'octobre': '10',
+            'novembre': '11',
+            'décembre': '12',
+        }
         self.lines = lines
+        self.account = None
+        # self.valueRightPosition = None
+        self.currentDate = None
+        self.currentTransactionLabel = None
 
     def parse(self):
         transactions = []
 
-        # for index, line in enumerate(self.lines):
+        for index, line in enumerate(self.lines):
+            if index == 0:
+                self.account = line[0]['value'] + ' N26'
+                continue
+
+            # if line[0]['value'] == 'Solde':
+            #     self.valueRightPosition = line[0]['x1']
+            #     continue
+
+            if self.isDateLine(line):
+                self.currentDate = self.extractDateFromLine(line)
+                continue
+
+            if self.currentDate is not None:
+
+                if self.lineIsTransactionLabel(line):
+                    self.currentTransactionLabel = line[0]['value']
+
+                if self.lineHasTransactionValue(line) and self.currentTransactionLabel is not None:
+                    transactions.append({
+                        'account': self.account,
+                        'date': self.currentDate,
+                        'label': self.currentTransactionLabel,
+                        'value': float(self.extractTransactionValue(line))
+                    })
+                    self.currentTransactionLabel = None
+
+            if line[0]['value'].startswith('VIR DE M'):
+                transactions[-1]['label'] = transactions[-1]['label'] + ' - ' + line[0]['value']
+
+            if line[0]['value'] == 'Vue d’ensemble':
+                break
 
         return transactions
+
+    def isDateLine(self, line):
+        return bool(re.match(self.dateRegex, line[0]['value']))
+
+    def extractDateFromLine(self, line):
+        matches = re.search(self.dateRegex, line[0]['value'])
+        day = int(matches.group(2))
+        month = matches.group(3)
+        year = int(matches.group(4))
+
+        return str(day) + '/' + self.months[month] + '/' + str(year)
+
+    def lineIsTransactionLabel(self, line):
+        return (line[0]['y0'] - line[0]['y1']) > 13
+
+    def lineHasTransactionValue(self, line):
+        for index, word in enumerate(line):
+            if bool(re.match(self.valueRegex, word['value'])):
+                return True
+
+        return False
+
+    def extractTransactionValue(self, line):
+        for index, word in enumerate(line):
+            if bool(re.match(self.valueRegex, word['value'])):
+                matches = re.search(self.valueRegex, word['value'])
+                sign = matches.group(1)
+                integers = matches.group(2)
+                decimals = matches.group(3)
+
+                return float(sign + integers + '.' + decimals)
--- tests/test_n26_statement_parser.py
+++ tests/test_n26_statement_parser.py
@@ -22,3 +22,108 @@ def testParse():
     with open('./tests/files/expected-results-n26.json') as file:
         expectedData = json.loads(file.read())
     assert transactions == expectedData
+
+def testIsDateLine():
+
+    n26parser = N26StatementParser(n26_lines)
+
+    line1 = [{ 'value': 'vendredi, 17. juillet 2020' }]
+    assert n26parser.isDateLine(line1) == True
+
+    line2 = [{ 'value': 'mercredi, 29. juillet 2020' }]
+    assert n26parser.isDateLine(line2) == True
+
+    line3 = [{ 'value': '15. juillet 2020 - 29. juillet 2020' }]
+    assert n26parser.isDateLine(line3) == False
+
+def testExtractDateFromLine():
+    n26parser = N26StatementParser(n26_lines)
+
+    line1 = [{ 'value': 'vendredi, 17. juillet 2020' }]
+    assert n26parser.extractDateFromLine(line1) == '17/07/2020'
+
+    line2 = [{ 'value': 'mercredi, 29. juillet 2020' }]
+    assert n26parser.extractDateFromLine(line2) == '29/07/2020'
+
+def testLineIsTransactionLabel():
+    n26parser = N26StatementParser(n26_lines)
+
+    line1 = [{
+        'value': 'M HECTOR MALONDA',
+        'x0': 30.0,
+        'y0': 597.05,
+        'x1': 162.29,
+        'y1': 583.91
+    }]
+    assert n26parser.lineIsTransactionLabel(line1) == True
+
+    line2 = [{
+        'value': 'NANTES MANGIN',
+        'x0': 30.0,
+        'y0': 485.98,
+        'x1': 114.79,
+        'y1': 472.84
+    }, {
+        'value': '-30,00€',
+        'x0': 523.28,
+        'y0': 478.25,
+        'x1': 565.26,
+        'y1': 463.47
+    }]
+    assert n26parser.lineIsTransactionLabel(line2) == True
+
+    line3 = [{
+        'value': 'Mastercard • Distributeur automatique',
+        'x0': 30.0,
+        'y0': 466.99,
+        'x1': 182.66,
+        'y1': 455.69
+    }]
+    assert n26parser.lineIsTransactionLabel(line3) == False
+
+    line4 = [{
+        'value': 'IBAN: FR7610278072280002032420139 • BIC: CMCIFR2AXXX',
+        'x0': 30.0,
+        'y0': 399.49,
+        'x1': 274.14,
+        'y1': 388.19
+    }]
+    assert n26parser.lineIsTransactionLabel(line4) == False
+
+def testLineHasTransationValue():
+    n26parser = N26StatementParser(n26_lines)
+
+    line1 = [{
+        'value': 'NANTES MANGIN',
+        'x0': 30.0,
+        'y0': 485.98,
+        'x1': 114.79,
+        'y1': 472.84
+    }, {
+        'value': '-30,00€',
+        'x0': 523.28,
+        'y0': 478.25,
+        'x1': 565.26,
+        'y1': 463.47
+    }]
+
+    assert n26parser.lineHasTransactionValue(line1) == True
+
+def testExtractTransationValue():
+    n26parser = N26StatementParser(n26_lines)
+
+    line1 = [{
+        'value': 'NANTES MANGIN',
+        'x0': 30.0,
+        'y0': 485.98,
+        'x1': 114.79,
+        'y1': 472.84
+    }, {
+        'value': '-30,05€',
+        'x0': 523.28,
+        'y0': 478.25,
+        'x1': 565.26,
+        'y1': 463.47
+    }]
+
+    assert n26parser.extractTransactionValue(line1) == -30.05
