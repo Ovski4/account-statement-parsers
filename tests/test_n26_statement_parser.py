c3142bf2edeaf09cb9ae0677b8e4ecef1ae583f7
Baptiste Bouchereau <baptiste.bouchereau@gmail.com>
2020-11-21 11:18:06 +0100

    Work on n26 parser v2
    Rename files
    
    Handle specific cases

--- tests/test_n26_statement_parser.py
+++ tests/test_n26_statement_parser.py
@@ -8,7 +8,8 @@ from n26_statement_parser import N26StatementParser
 from pdf_parser import PdfParser
 
 sys.path.append('./tests/files')
-from releve_n26 import n26_lines
+from releve_n26_v2_1 import n26_v2_1_lines
+from releve_n26_v2_2 import n26_v2_2_lines
 
 if os.environ.get('DEBUG') == 'true':
     import ptvsd
@@ -17,113 +18,36 @@ if os.environ.get('DEBUG') == 'true':
 
 def testParse():
 
-    n26parser = N26StatementParser(n26_lines)
+    n26parser = N26StatementParser(n26_v2_1_lines)
     transactions = n26parser.parse()
-    with open('./tests/files/expected-results-n26.json') as file:
+    with open('./tests/files/expected-results-n26-v2-1.json') as file:
         expectedData = json.loads(file.read())
     assert transactions == expectedData
 
-def testIsDateLine():
+    n26parser = N26StatementParser(n26_v2_2_lines)
+    transactions = n26parser.parse()
+    with open('./tests/files/expected-results-n26-v2-2.json') as file:
+        expectedData = json.loads(file.read())
+    assert transactions == expectedData
 
-    n26parser = N26StatementParser(n26_lines)
+def testLinesAreNotWithinBoundaries():
 
-    line1 = [{ 'value': 'vendredi, 17. juillet 2020' }]
-    assert n26parser.isDateLine(line1) == True
+    word1 = {
+		'value': '27.09.2020',
+		'x0': 389.1,
+		'y0': 372.64,
+		'x1': 448.03,
+		'y1': 357.53
+	}
 
-    line2 = [{ 'value': 'mercredi, 29. juillet 2020' }]
-    assert n26parser.isDateLine(line2) == True
+    word2 = {
+		'value': '27.09.2020',
+		'x0': 389.1,
+		'y0': 372.64,
+		'x1': 448.03,
+		'y1': 357.53
+	}
 
-    line3 = [{ 'value': '15. juillet 2020 - 29. juillet 2020' }]
-    assert n26parser.isDateLine(line3) == False
-
-def testExtractDateFromLine():
-    n26parser = N26StatementParser(n26_lines)
-
-    line1 = [{ 'value': 'vendredi, 17. juillet 2020' }]
-    assert n26parser.extractDateFromLine(line1) == '17/07/2020'
-
-    line2 = [{ 'value': 'mercredi, 29. juillet 2020' }]
-    assert n26parser.extractDateFromLine(line2) == '29/07/2020'
-
-def testLineIsTransactionLabel():
-    n26parser = N26StatementParser(n26_lines)
-
-    line1 = [{
-        'value': 'M HECTOR MALONDA',
-        'x0': 30.0,
-        'y0': 597.05,
-        'x1': 162.29,
-        'y1': 583.91
-    }]
-    assert n26parser.lineIsTransactionLabel(line1) == True
-
-    line2 = [{
-        'value': 'NANTES MANGIN',
-        'x0': 30.0,
-        'y0': 485.98,
-        'x1': 114.79,
-        'y1': 472.84
-    }, {
-        'value': '-30,00€',
-        'x0': 523.28,
-        'y0': 478.25,
-        'x1': 565.26,
-        'y1': 463.47
-    }]
-    assert n26parser.lineIsTransactionLabel(line2) == True
-
-    line3 = [{
-        'value': 'Mastercard • Distributeur automatique',
-        'x0': 30.0,
-        'y0': 466.99,
-        'x1': 182.66,
-        'y1': 455.69
-    }]
-    assert n26parser.lineIsTransactionLabel(line3) == False
-
-    line4 = [{
-        'value': 'IBAN: FR7610278072280002032420139 • BIC: CMCIFR2AXXX',
-        'x0': 30.0,
-        'y0': 399.49,
-        'x1': 274.14,
-        'y1': 388.19
-    }]
-    assert n26parser.lineIsTransactionLabel(line4) == False
-
-def testLineHasTransationValue():
-    n26parser = N26StatementParser(n26_lines)
-
-    line1 = [{
-        'value': 'NANTES MANGIN',
-        'x0': 30.0,
-        'y0': 485.98,
-        'x1': 114.79,
-        'y1': 472.84
-    }, {
-        'value': '-30,00€',
-        'x0': 523.28,
-        'y0': 478.25,
-        'x1': 565.26,
-        'y1': 463.47
-    }]
-
-    assert n26parser.lineHasTransactionValue(line1) == True
-
-def testExtractTransationValue():
-    n26parser = N26StatementParser(n26_lines)
-
-    line1 = [{
-        'value': 'NANTES MANGIN',
-        'x0': 30.0,
-        'y0': 485.98,
-        'x1': 114.79,
-        'y1': 472.84
-    }, {
-        'value': '-30,05€',
-        'x0': 523.28,
-        'y0': 478.25,
-        'x1': 565.26,
-        'y1': 463.47
-    }]
-
-    assert n26parser.extractTransactionValue(line1) == -30.05
+    n26parser = N26StatementParser([])
+    assert n26parser.linesAreWithinBoundaries(word1, {'x0': 63.36, 'x1': 80.7}) == False
+    assert n26parser.linesAreWithinBoundaries(word2, {'x0': 390.36, 'x1': 400}) == True
