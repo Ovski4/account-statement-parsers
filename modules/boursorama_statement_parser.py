716a80b602ec67a86a2f11b7cfffb8e139757ce4
Baptiste Bouchereau <baptiste.bouchereau@gmail.com>
2020-12-02 16:05:38 +0100

    Work on the boursorama statement parser

--- modules/boursorama_statement_parser.py
+++ modules/boursorama_statement_parser.py
@@ -1,4 +1,5 @@
 import sys
+import re
 sys.path.append('./modules')
 from line_reader import LineReader
 from pdf_parser import PdfParser
@@ -7,8 +8,113 @@ class BoursoramaStatementParser:
 
     def __init__(self, lines):
         self.lines = lines
+        self.account = None
+        self.columnBoundaries = {
+            'label': {
+                'x0': 82,
+                'x1': 275
+            },
+            'date': {
+                'x0': 32,
+                'x1': 81
+            },
+            # 'debit': {
+            #     'x0': ,
+            #     'x1': 
+            # },
+            'credit': {
+                'x0': 453,
+                'x1': 536
+            }
+        }
 
     def parse(self):
         transactions = []
 
+        for index, line in enumerate(self.lines):
+            if self.isBankAccountLine(line):
+                self.account = self.extractBankAccount(line)
+                continue
+
+            if self.isCreditLine(line):
+                transactions.append(self.extractTransaction(index, self.lines, 'credit'))
+
         return transactions
+
+    # def isBankBalanceLine(self, line):
+    #     if len(line) != 3:
+    #         return False
+
+    #     if (line[0]['value'] != 'SOLDE AU :'):
+    #         return False
+
+    #     return True
+
+    def isLabelWord(self, word):
+        return self.wordIsWithinBoundaries(word, self.columnBoundaries['label'])
+
+    def isDateWord(self, word):
+        return self.wordIsWithinBoundaries(word, self.columnBoundaries['date'])
+
+    def isDebitWord(self, word):
+        return self.wordIsWithinBoundaries(word, self.columnBoundaries['debit'])
+
+    def isCreditWord(self, word): 
+        return self.wordIsWithinBoundaries(word, self.columnBoundaries['credit'])
+
+    def wordIsWithinBoundaries(self, word, boundary):
+        return word['x0'] >= boundary['x0'] and word['x1'] <= boundary['x1']
+
+    def isCreditLine(self, line):
+        if len(line) != 4:
+            return False
+
+        if self.isDateWord(line[0]) and self.isLabelWord(line[1]) and self.isCreditWord(line[3]):
+            return True
+
+        return False
+
+    def isDate(self, value):
+        dateRegex = r'\d{1,2}\/\d{2}\/\d{4}'
+        return bool(re.match(dateRegex, value))
+
+    def isBankAccountLine(self, line):
+        if len(line) != 11:
+            return False
+
+        if (self.isDate(line[0]['value']) and
+            line[1]['value'].isdigit() and
+            line[2]['value'].isdigit() and
+            line[3]['value'].isdigit() and
+            line[4]['value'].isdigit() and
+            (not line[5]['value'].isdigit())):
+            return True
+
+        return False
+
+    def extractBankAccount(self, line):
+        return line[1]['value'] + ' ' + line[2]['value'] + ' ' + line[3]['value'] + ' ' + line[4]['value']
+
+    def extractTransaction(self, lineIndex, lines, transactionType):
+
+        value = float(
+          lines[lineIndex][3]['value']
+          .replace('.', '')
+          .replace(',', '.')
+        )
+
+        transaction = {
+            'account': self.account,
+            'date': lines[lineIndex][0]['value'],
+            'label': lines[lineIndex][1]['value'],
+            'value': value if transactionType == 'credit' else -value
+        }
+
+        startIndex = lineIndex + 1
+        for line in lines[startIndex:]:
+            if len(line) == 1 and self.isLabelWord(line[0]):
+                transaction['label'] = transaction.get('label') + ' - ' + line[0]['value']
+            else:
+                break
+
+        return transaction
--- tests/test_boursorama_statement_parser.py
+++ tests/test_boursorama_statement_parser.py
@@ -22,3 +22,109 @@ def testParse():
     with open('./tests/files/expected-results-boursorama.json') as file:
         expectedData = json.loads(file.read())
     assert transactions == expectedData
+
+# pdfFile = open('/usr/src/app/tests/files/releve-boursorama.pdf', 'rb')
+# lines = PdfParser().parse(pdfFile)
+# f = open( '/usr/src/app/tests/files/file.py', 'w' )
+# f.write( 'lines = ' + repr(lines) + '\n' )
+# f.close()
+# pdfFile.close()
+
+def testIsLabelWord():
+
+    lines = [
+        { 'value': 'VIR VIREMENT CREATION COMPTE', 'x0': 82.8, 'y0': 500.86, 'x1': 217.2, 'y1': 492.45},
+        {'value': 'BOURSORAMA', 'x0': 82.8, 'y0': 488.86, 'x1': 145.2, 'y1': 480.45},
+        {'value': 'BRS80', 'x0': 82.8, 'y0': 476.86, 'x1': 111.6, 'y1': 468.45},
+        {'value': 'Réf : DDDDFRPP847C0120230829105811111', 'x0': 82.8, 'y0': 392.86, 'x1': 274.8, 'y1': 384.45},
+        {'value': 'Nouveau solde en EUR :', 'x0': 158.4, 'y0': 208.85, 'x1': 262.42, 'y1': 198.14},
+        {'value': '2.553,00', 'x0': 439.2, 'y0': 206.86, 'x1': 535.2, 'y1': 198.45}
+    ]
+
+    parser = BoursoramaStatementParser(lines)
+    assert parser.isLabelWord(lines[0]) == True
+    assert parser.isLabelWord(lines[1]) == True
+    assert parser.isLabelWord(lines[2]) == True
+    assert parser.isLabelWord(lines[3]) == True
+    assert parser.isLabelWord(lines[4]) == True
+    assert parser.isLabelWord(lines[5]) == False
+
+def testIsDateWord():
+
+    lines = [
+        {'value': '18/08/2020', 'x0': 32.4, 'y0': 500.86, 'x1': 80.4, 'y1': 492.45},
+        {'value': '18/08/2020', 'x0': 306.0, 'y0': 500.86, 'x1': 354.0, 'y1': 492.45},
+        {'value': '2.553,00', 'x0': 439.2, 'y0': 206.86, 'x1': 535.2, 'y1': 198.45}
+    ]
+
+    parser = BoursoramaStatementParser(lines)
+    assert parser.isDateWord(lines[0]) == True
+    assert parser.isDateWord(lines[1]) == False
+    assert parser.isDateWord(lines[2]) == False
+
+def testIsCreditWord():
+
+    lines = [
+        {'value': '18/08/2020', 'x0': 32.4, 'y0': 500.86, 'x1': 80.4, 'y1': 492.45},
+        {'value': '1.473,00', 'x0': 453.6, 'y0': 464.86, 'x1': 535.2, 'y1': 456.45}
+    ]
+
+    parser = BoursoramaStatementParser(lines)
+    assert parser.isCreditWord(lines[0]) == False
+    assert parser.isCreditWord(lines[1]) == True
+
+def testIsCreditLine():
+    line =[
+        {
+            'value': '31/08/2020',
+            'x0': 32.4,
+            'y0': 416.86,
+            'x1': 80.4,
+            'y1': 408.45
+        }, {
+            'value': 'VIR SEPA MLLE DEBONARD HELENE',
+            'x0': 82.8,
+            'y0': 416.86,
+            'x1': 217.2,
+            'y1': 408.45
+        }, {
+            'value': '31/08/2020',
+            'x0': 306.0,
+            'y0': 416.86,
+            'x1': 354.0,
+            'y1': 408.45
+        }, {
+            'value': '700,00',
+            'x0': 453.6,
+            'y0': 416.86,
+            'x1': 535.2,
+            'y1': 408.45
+        }
+    ]
+
+    parser = BoursoramaStatementParser([line])
+    assert parser.isCreditLine(line) == True
+
+def testIsBankAccountLine():
+    line = [
+        {'value': '2/09/2020', 'x0': 32.4, 'y0': 574.54, 'x1': 74.93, 'y1': 564.14},
+        {'value': '40618', 'x0': 82.8, 'y0': 574.54, 'x1': 107.82, 'y1': 564.14},
+        {'value': '83333', 'x0': 118.8,'y0': 574.54, 'x1': 143.82,'y1': 564.14},
+        {'value': '00340977444','x0': 147.6,'y0': 574.54, 'x1': 202.64,'y1': 564.14},
+        {'value': '58','x0': 216.0,'y0': 574.54, 'x1': 226.01,'y1': 564.14},
+        {'value': 'EUR','x0': 234.0,'y0': 574.54, 'x1': 253.0,'y1': 564.14},
+        {'value': '18/08/2020','x0': 270.0,'y0': 574.54, 'x1': 315.04,'y1': 564.14},
+        {'value': '31/08/2020','x0': 334.8,'y0': 574.54, 'x1': 379.84,'y1': 564.14},
+        {'value': '300,00 ¤','x0': 388.8,'y0': 574.54, 'x1': 438.84,'y1': 564.14},
+        {'value': '0,000000 %','x0': 457.2,'y0': 574.54, 'x1': 510.24,'y1': 564.14},
+        {'value': '1','x0': 514.8,'y0': 574.54, 'x1': 527.31,'y1': 564.14 }
+    ]
+
+    parser = BoursoramaStatementParser([line])
+    assert parser.isBankAccountLine(line) == True
+
+def testIsDate():
+    parser = BoursoramaStatementParser([])
+    assert parser.isDate('2/09/2020') == True
+    assert parser.isDate('31/08/2020') == True
+    assert parser.isDate('3a/08/2020') == False
