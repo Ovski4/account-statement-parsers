24cc16d34ebfea8cc15d0978028b93ab5d664764
Baptiste Bouchereau <baptiste.bouchereau@gmail.com>
2020-12-11 14:07:16 +0100

    Add a balance endpoint

--- run_api.py
+++ run_api.py
@@ -6,7 +6,9 @@ from datetime import datetime
 sys.path.append('modules')
 from credit_mutuel_statement_parser import CreditMutuelStatementParser
 from caisse_epargne_statement_parser import CaisseEpargneStatementParser
+from functools import reduce
 from n26_statement_parser import N26StatementParser
+from boursorama_statement_parser import BoursoramaStatementParser
 from pdf_parser import PdfParser
 from klein import run, route
 
@@ -20,6 +22,8 @@ def parse(file_path, parser_name):
         parser = CaisseEpargneStatementParser(lines)
     elif (parser_name == 'n26'):
         parser = N26StatementParser(lines)
+    elif (parser_name == 'boursorama'):
+        parser = BoursoramaStatementParser(lines)
     else:
         raise Exception('Unknown parser with name' + parser_name)
 
@@ -27,14 +31,31 @@ def parse(file_path, parser_name):
 
     return transactions
 
-def generate_response(request, parser_name):
+def dump_transactions(request, parser_name):
     file_path = request.args[b'statement'][0]
     transactions = parse(file_path, parser_name)
 
     return json.dumps(transactions, indent=2, ensure_ascii=False)
 
+def dump_balance(request, parser_name):
+
+    def add_transaction_value(a, b):
+        if isinstance(a ,dict):
+            a = a['value']
+        return round(a + b['value'], 2)
+
+    file_path = request.args[b'statement'][0]
+    transactions = parse(file_path, parser_name)
+    total = reduce(add_transaction_value, transactions)
+
+    return json.dumps({'total': total}, indent=2, ensure_ascii=False)
+
 @route('/<parser_name>')
 def statement(request, parser_name):
-    return generate_response(request, parser_name)
+    return dump_transactions(request, parser_name)
+
+@route('/<parser_name>/balance')
+def balance(request, parser_name):
+    return dump_balance(request, parser_name)
 
 run('0.0.0.0', 80)
